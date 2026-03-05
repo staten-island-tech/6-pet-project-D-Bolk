1ee9c683a277d4899eb1c5a68810396e7820202f
D-Bolk <danielb643@nycstudents.net>
2026-03-05 09:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@
 
 Calculator.add(5, 6) """
 
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -25,6 +25,35 @@ class Hero:
 Jillian = Hero("Jillian", 150, ["Potion"])
 Jillian.buy({"title": "Sword", "atk": 34})
 print(Jillian.__dict__)
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}") """
+
+
+
+
+
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+Whalen = Hero("Whalen", 150, ["Watch"])
+Whalen.buy({"Teacher": "ButtonDown", "atk": 34})
+print(Whalen.__dict__)
+
 class BankAccount:
     def __init__(self, owner, balance):
         self.owner = owner
