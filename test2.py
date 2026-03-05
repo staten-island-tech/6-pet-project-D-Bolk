e05bc959bc49b78e2e9dfa0ba2565914565cac27
D-Bolk <danielb643@nycstudents.net>
2026-03-05 09:05:24 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,26 +4,34 @@
     for letter in x:
  """
 
-import sys
-
-def solve():
-    word = sys.stdin.read().strip()
-    
-    target = "HONI"
-    
-    honi_idx = 0
-    
+""" def solve():
+    word = honi
+    honi = 0
     count = 0
     
     for letter in word:
-        if letter == target[honi_idx]:
-            honi_idx += 1
+        if letter==word[honi]:
+            honi += 1
             
-            if honi_idx == 4:
+            if honi == 4:
                 count += 1
-                honi_idx = 0
+                honi = 0
                 
     print(count)
 
-if __name__ == '__main__':
-    solve()
+    solve() """
+def magnus(word):
+    count = 0
+    state = 0
+for char in word:
+    if state == 0 and char.upper() == 'H':
+        state == 1
+    elif state == 1 and char.upper() == 'O'
+        state = 2
+    elif state == 2 and char.upper() == 'N':
+        state = 3
+    elif state == 3 and char.upper() == 'I':
+        count +=1
+        state = 0
+
+print(count)
